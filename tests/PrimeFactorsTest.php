011412b85aef999e87d8c9a4a6f98958fd9c114c
Clebson Moura <clebson.sh@gmail.com>
2024-02-14 13:58:30 -0300

    Prime Factors Kata

---
+++ tests/PrimeFactorsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\PrimeFactors;
+
+it('returns a array of prime factors', function (int $number, array $expected) {
+    expect((new PrimeFactors())->generate($number))->toBe($expected);
+})->with('factors');
+
+dataset('factors', [
+    [1, []],
+    [2, [2]],
+    [3, [3]],
+    [4, [2, 2]],
+    [5, [5]],
+    [6, [2, 3]],
+    [7, [7]],
+    [8, [2, 2, 2]],
+    [9, [3, 3]],
+    [100, [2, 2, 5, 5]],
+    [999, [3, 3, 3, 37]],
+    [1000, [2, 2, 2, 5, 5, 5]],
+]);
