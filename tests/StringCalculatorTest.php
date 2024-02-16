a10a5cbba8453faa38dd75468341d66d0e416b93
Clebson Moura <clebson.sh@gmail.com>
2024-02-16 11:00:47 -0300

    Add test for summing one number

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -7,3 +7,9 @@ it('evaluates a empty string as 0', function () {
 
     expect($calculator->add(''))->toBe(0);
 });
+
+it('finds the sum of one number', function () {
+    $calculator = new StringCalculator();
+
+    expect($calculator->add('5'))->toBe(5);
+});
