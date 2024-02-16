66f7fac66c985d1a2b950b8933702219ee6597d4
Clebson Moura <clebson.sh@gmail.com>
2024-02-16 11:25:25 -0300

    Add negative number validation in StringCalculator class

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -12,6 +12,12 @@ class StringCalculator
 
         $numbers = preg_split('/,|\n/', $numbers);
 
+        foreach ($numbers as $number) {
+            if ($number < 0) {
+                throw new \Exception('Negatives not allowed ' . $number);
+            }
+        }
+
         return (int) array_sum($numbers);
     }
 }
