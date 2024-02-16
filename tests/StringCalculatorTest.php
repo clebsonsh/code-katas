7067239c20d019a1fb55a83c70e4bc27526527e3
Clebson Moura <clebson.sh@gmail.com>
2024-02-16 11:16:06 -0300

    Add tests for finding the sum of any amount of numbers

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -19,3 +19,16 @@ it('finds the sum of two numbers', function () {
 
     expect($calculator->add('5,5'))->toBe(10);
 });
+
+it('finds the sum of any amount of numbers', function (string $numbers, int $sum) {
+    $calculator = new StringCalculator();
+
+    expect($calculator->add($numbers))->toBe($sum);
+})->with('numbers');
+
+dataset('numbers', [
+    ['1,2,3,4,5,6,7,8,9,10', 55],
+    ['1,2,3,4,5,6,7,8,9,10,11,12,13,14,15', 120],
+    ['1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20', 210],
+    ['1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25', 325],
+]);
